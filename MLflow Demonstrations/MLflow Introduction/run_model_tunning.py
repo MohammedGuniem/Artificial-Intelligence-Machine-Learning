0638d75597e477cb06c813dadc0708baeaa51d85
Guniem, Mohammed <mghunime@yahoo.no>
2024-08-03 18:19:27 +0200

    The run_tunned_model.py script is finsihed
    Need some documentation

--- MLflow Demonstrations/MLflow Introduction/run_model_tunning.py
+++ MLflow Demonstrations/MLflow Introduction/run_model_tunning.py
@@ -1,43 +1,44 @@
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_predict, KFold
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 import mlflow.sklearn
 import pandas as pd
+import numpy as np
 import mlflow
 
-## Set Tracking Server
+# Set Tracking Server
 mlflow.set_tracking_uri("http://localhost:5000")
 
-## Create or Set the experiment 
+# Create or Set the experiment 
 mlflow.set_experiment(experiment_name="Titanic Experiments")
 
-## Tag new experiment
+# Tag new experiment
 mlflow.set_experiment_tag(key="version", value="1.0")
 
-## Set Auto logging for Scikit-learn flavor 
-mlflow.sklearn.autolog()
-# You can also log all metrics (Not just those for Scikit-learn flavor) by using
+# You can set auto logging for all metrics by using
 #mlflow.autolog()
 
-## Load dataset
+# You can also set auto logging for Scikit-learn flavor by using
+#mlflow.sklearn.autolog()
+
+# Disable autologging
+mlflow.autolog(disable=True)
+
+# Load dataset
 df = pd.read_csv('data/titanic.csv')
+
+# Create male column with 1 for male and 0 for female
 df['male'] = df['Sex'] == 'male'
+
+# Extract features
 X = df[['Pclass', 'male', 'Age', 'Siblings/Spouses', 'Parents/Children', 'Fare']].values
+# Extract corresponding targets
 y = df['Survived'].values
 
-## Start an MLflow run
+## Start a New MLflow Run
 with mlflow.start_run() as run:
 
-    ## Randomly split data
-    test_size = 0.2
-    random_state = 99
-    # Log the values of split metrics split_test_size, split_random_state
-    mlflow.log_metric("split_test_size", test_size) 
-    mlflow.log_metric("split_random_state", random_state)
-    # Split data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
-
-    ## Define the parameter grid
+    # Define the tunning parameter grid
     param_grid = {
         'criterion': ['gini', 'entropy'],
         'max_depth': [5, 15, 25, 35, 45],
@@ -45,72 +46,88 @@ with mlflow.start_run() as run:
         'max_leaf_nodes': [10, 20, 35, 50]
     }
 
-    ## Create a Decision Tree classifier
-    clf = DecisionTreeClassifier()
+    # Create a Decision Tree classifier with random_state
+    clf = DecisionTreeClassifier(random_state=99)
 
-    ## Perform Grid Search with cross-validation
+    # Perform grid search with cross-validation
     cv = 5
     scoring='f1'
+    # Set up GridSearchCV with random_state in the cv strategy
+    cv_strategy = KFold(n_splits=5, shuffle=True, random_state=99)
     grid_search = GridSearchCV(
         estimator=clf, 
-        param_grid=param_grid, 
-        cv=cv, 
-        scoring=scoring
+        param_grid=param_grid,
+        scoring=scoring,
+        cv=cv_strategy
     )
 
     ## Fit the grid search to the data
-    grid_search.fit(X_train, y_train)
+    grid_search.fit(X, y)
 
-    # Print the number of unique hyperparameter combinations and the total runs
-    total_combinations = len(grid_search.cv_results_['params'])
-    total_runs = total_combinations * grid_search.cv
-    print(f"Total unique hyperparameter combinations: {total_combinations}")
-    print(f"Total number of models trained (considering CV folds): {total_runs}")
+    ## Log Parameters
+    # Log the parameter cv that stores how many k-fold of data the tunning performed on
+    print("cross_validation_splits: ", cv)
+    mlflow.log_param("cross_validation_splits", cv)
+    
+    # Log the configured tunning parameters
+    for parameter, value in param_grid.items():
+        print(f"{parameter}: ", value)
+    mlflow.log_params(param_grid)
 
-    ## Get the best parameters from grid search
+    # Get the best tunning parameters from grid search
     best_params = grid_search.best_params_
-    print(f"Best Parameters: {best_params}")
+    # Log the best tunning parameters with prefix "optimal_tunned_"
+    prefix = "optimal_tunned"
+    for parameter, value in best_params.items():
+        print(f"{prefix}_{parameter}: ", value)
+        mlflow.log_param(f"{prefix}_{parameter}", value)
 
-    ## Get model with best model and log its evaluation metrics
-    best_clf = grid_search.best_estimator_
-    # Predict with best model
-    y_pred = best_clf.predict(X_test)
-    # Get Accuracy Score
-    accuracy = accuracy_score(y_test, y_pred)
-    # Get Precision Score
-    precision = precision_score(y_test, y_pred)
-    # Get Recall Score
-    recall = recall_score(y_test, y_pred)
-    # Get F1 Score
-    f1 = f1_score(y_test, y_pred)
-    # Get Confusion Matrix
-    cm = confusion_matrix(y_test, y_pred)
-    # Extract and log false negatives, true negatives, false positives, true positives
-    tn, fp, fn, tp = cm.ravel()
-    cm_metrics = {
-        "evaluation_true_negatives": tn,
-        "evaluation_false_positives": fp,
-        "evaluation_false_negatives": fn,
-        "evaluation_true_positives": tp
-    }
+    ## Log metrics
 
-    ## Log parameters
-    # Log the parameter cv that stores how many k-fold of data the tunning performed on.
-    mlflow.log_param("cv", cv)
-    # Log the parameter scoring_measure that stores what scoring measure is used to find the optimal tunned model.
-    mlflow.log_param("scoring_measure", scoring)
-    # Log multiple best tunning parameters at once
-    mlflow.log_params(best_params)
+    # Log the best scoring of grid search
+    gs_best_score = grid_search.best_score_
+    print(f"grid_search_best_{scoring}_score", gs_best_score)
+    mlflow.log_metric(f"grid_search_best_{scoring}_score", gs_best_score)
+    
+    # Get the best model
+    best_model = grid_search.best_estimator_
 
-    # Log the value of the evaluation metrics from the best classifer
-    mlflow.log_metric("evaluation_accuracy", accuracy)
-    mlflow.log_metric("evaluation_precision", precision)
-    mlflow.log_metric("evaluation_recall", recall)
-    mlflow.log_metric("evaluation_f1_score", f1)
-    # Log the values of the confusion matrix from best classifer all 4 at once
-    mlflow.log_metrics(cm_metrics)
+    # Perform cross_val_predict with the same random_state in the cv strategy
+    y_pred = cross_val_predict(best_model, X, y, cv=cv_strategy)
+    
+    metrics = {}
 
-    # Get the run_id
+    # Calculate accuracy score
+    accuracy = accuracy_score(y, y_pred)
+    metrics["accuracy"] = accuracy
+
+    # Calculate recall score
+    recall = recall_score(y, y_pred, average='macro')
+    metrics["recall"] = recall
+
+    # Calculate precision score
+    precision = precision_score(y, y_pred, average='macro')
+    metrics["precision"] = precision
+
+    # Calculate f1 score
+    f1 = f1_score(y, y_pred, average='macro')
+    metrics["f1"] = f1
+
+    # Get confusion matrix
+    tn, fp, fn, tp = confusion_matrix(y, y_pred).ravel()
+    metrics["cm_true_positives"] = tp
+    metrics["cm_false_negatives"] = fn
+    metrics["cm_false_positives"] = fp
+    metrics["cm_true_negatives"] = tn
+
+    # Print evaluation metrics
+    for metric, value in metrics.items():
+        print(f"{metric}: ", value)
+
+    # Log evaluation metrics
+    mlflow.log_metrics(metrics)
+
+    # Get and log the ID of this run as parameter
     run_id = run.info.run_id
-    print(f"RUN_ID: {run_id}")
-    print(f"Best F1 Score: {f1}")
+    mlflow.log_param("run_id", run_id)
+    print(f"run_id: {run_id}")
